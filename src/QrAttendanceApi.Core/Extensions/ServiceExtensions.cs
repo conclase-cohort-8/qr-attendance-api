abf7f90668d18a671e143f57bdab946590d381da
Toba Ojo <ojotobar@gmail.com>
2025-12-23 18:05:11 +0100

    Refactor

--- src/QrAttendanceApi.Core/Extensions/ServiceExtensions.cs
+++ src/QrAttendanceApi.Core/Extensions/ServiceExtensions.cs
@@ -2,7 +2,6 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc.Versioning;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using QrAttendanceApi.Application.Abstractions;
