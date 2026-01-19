311ceaaddcaeb692902f6ab6cb246dd39db62820
Tim <timothytemi@gmail.com>
2026-01-19 14:44:37 +0100

    Implemented QRSession-TokenSecurityLayer

--- src/QrAttendanceApi.Core/Controllers/V1/QrSessionsController.cs
+++ src/QrAttendanceApi.Core/Controllers/V1/QrSessionsController.cs
@@ -1,5 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
+using QrAttendanceApi.Application.Commands.QRs;
+using QrAttendanceApi.Application.DTOs;
 using QrAttendanceApi.Application.Services.Abstractions;
+using QrAttendanceApi.Core.Controllers.Extensions;
+using QrAttendanceApi.Infrastructure.ExternalServices;
 
 namespace QrAttendanceApi.Core.Controllers.V1
 {
@@ -14,5 +21,73 @@ namespace QrAttendanceApi.Core.Controllers.V1
         {
             _manager = manager;
         }
+
+
+
+        [HttpPost]
+        [Authorize(Roles = "Staff,Admin,SuperAdmin")]
+        public async Task<IActionResult> Post([FromBody] CreateQrSessionCommand command)
+        {
+            var loggedInUserId = HttpContext.GetLoggedInUserId();
+            var result = await _manager.QrSession.Create(loggedInUserId, command);
+            if (!result.Success)
+            {
+                return ProcessError(result);
+            }
+            return Ok(result.GetResult<CreateQrResponseDto>);
+
+        }
+
+        [HttpPost("{id}/token")]
+        [Authorize(Roles = "Staff,Admin,SuperAdmin")]
+
+        public async Task<IActionResult> GenerateQr([FromRoute] Guid id)
+        {
+            var loggedInUserId = HttpContext.GetLoggedInUserId();
+            var result = await _manager.QrSession.GenerateQrToken(loggedInUserId, id);
+            if (!result.Success)
+            {
+                return ProcessError(result);
+            }
+            return Ok(result.GetResult<QrTokenDto>());
+
+
+        }
+
+        [HttpPost("{id}/token-Image")]
+        [Authorize(Roles = "Staff,Admin,SuperAdmin")]
+
+        public async Task<IActionResult> GenerateQrPng([FromRoute] Guid id)
+        {
+            var loggedInUserId = HttpContext.GetLoggedInUserId();
+            var result = await _manager.QrSession.GenerateQrToken(loggedInUserId, id);
+            if (!result.Success)
+            {
+                return ProcessError(result);
+            }
+            var token = result.GetResult<QrTokenDto>();
+            var qrByts = QrCodeHelper.Generate(token.Data!.Token);
+            return File(qrByts,"Image/png");
+
+
+        }
+
+
+        [HttpPost("{id}/validate")]
+        [Authorize]
+
+        public async Task<IActionResult> ValidateQr([FromRoute] Guid id, SessionAttendanceCommand command)
+        {
+            var loggedInUserId = HttpContext.GetLoggedInUserId();
+            var result = await _manager.Attendance.MarkAttendance(loggedInUserId, id, command);
+            if (!result.Success)
+            {
+                return ProcessError(result);
+            }
+            return Ok(result.GetResult<QrTokenDto>());
+
+
+        }
     }
+
 }
